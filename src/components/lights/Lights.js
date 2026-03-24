a6a6432b83e3fe31f277e280ece7c89b4902c91c
Nayomekaia <nayomedoelwijt@gmail.com>
2026-03-24 14:03:22 +0100

    verlichting toegevoegd

--- src/components/lights/Lights.js
+++ src/components/lights/Lights.js
@@ -1,1 +1,14 @@
 // verlichting op object
+import * as THREE from 'three';
+
+export function createLights(scene) {
+  const light = new THREE.DirectionalLight(0xffffff, 2);
+  light.position.set(5, 5, 5);
+
+  const ambient = new THREE.AmbientLight(0xffffff, 0.5);
+
+  scene.add(light);
+  scene.add(ambient);
+
+  return { update: () => {} };
+}
