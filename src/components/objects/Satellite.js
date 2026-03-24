8e2467b871dddd141abda32b0d098ed3d1bccc0b
Nayomekaia <nayomedoelwijt@gmail.com>
2026-03-24 14:02:02 +0100

    object gemaakt

--- src/components/objects/Satellite.js
+++ src/components/objects/Satellite.js
@@ -1,1 +1,24 @@
-// object zelf dus de sataliet 
+// Satellite.js
+import * as THREE from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+
+let satellite; 
+
+export function loadSatellite(scene) {
+  const loader = new OBJLoader();
+
+  loader.load(
+    '/satellite.obj', 
+    (object) => {
+      satellite = object;
+
+      //voeg toe aan scene
+      scene.add(object);
+    },
+    undefined,
+    (error) => {
+      console.error('Error loading satellite OBJ:', error);
+    }
+  );
+}
+
