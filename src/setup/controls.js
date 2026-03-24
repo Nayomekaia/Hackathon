bdcb48add4349fa3c6fda5fc35924f60431c8f77
Nayomekaia <nayomedoelwijt@gmail.com>
2026-03-24 14:03:41 +0100

    contorls toegevoegd
    controls toegevoegd voor drag

---
+++ src/setup/controls.js
@@ -0,0 +1,19 @@
+// controls 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+let controls;
+
+export function createControls(camera, renderer) {
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  controls.enableDamping = true;
+  controls.enableZoom = true;    
+  controls.enablePan = false;    
+
+  return controls;
+}
+
+
+export function updateControls() {
+  if (controls) controls.update();
+}
