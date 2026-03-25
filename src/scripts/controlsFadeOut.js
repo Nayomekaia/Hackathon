22a7a1c0481eeb8e715309103b64bff254037953
vsheo <vireshsheoratan@gmail.com>
2026-03-25 16:37:19 +0100

    feat: no img interaction #24

--- src/scripts/controlsFadeOut.js
+++ src/scripts/controlsFadeOut.js
@@ -3,19 +3,14 @@ const toggleControls = document.querySelector(".show-controls");
 const componentPicker = document.querySelector(".component-picker");
 const canvas = document.querySelector("#app");
 
-//  fade out bij klik van de controls
-componentPicker.addEventListener("click", () => {
-  if (container) {
-    container.classList.add("hide");
-    toggleControls.classList.remove("hide");
-  }
-});
-
-canvas.addEventListener("click", () => {
-  if (container) {
-    container.classList.add("hide");
-    toggleControls.classList.remove("hide");
-  }
+// if there is a click event inside the component-picker or inside the canvas the the controls dissapear
+[componentPicker, canvas].forEach(el => {
+  el.addEventListener("click", () => {
+    if (container) {
+      container.classList.add("hide");
+      toggleControls.classList.remove("hide");
+    }
+  });
 });
 
 toggleControls.addEventListener("click", () => {
