cbe05544fa46cce46e159e849afee723ad4fbc01
Melissa <melissa.blankenburgh@hva.nl>
2026-03-25 15:18:52 +0100

    dropdown werkt nu*
    tijd om z-index te fixen

---
+++ src/scripts/dropdown.js
@@ -0,0 +1,6 @@
+function toggleDropdown(){
+    document.getElementById("form").classList.toggle("dropdown-shown");
+}
+
+
+// document.getElementsByClassName(component-picker).addEventListener("mousedown", document.getElementById("form").classList.remove("dropdown-shown"));
