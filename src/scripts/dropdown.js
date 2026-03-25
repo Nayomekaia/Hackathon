c6bebba8d8b159b1dc8c78d54e26063b0afd7a54
Melissa <melissa.blankenburgh@hva.nl>
2026-03-25 16:15:41 +0100

    responsive gefixt: form heeft weer een id
    plus de infobox wordt niet meer verborgen hij het tevoorschijn halen van de dropdown

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -1,6 +1,5 @@
 function toggleDropdown(){
     document.getElementById("form").classList.toggle("dropdown-shown");
-    document.querySelector(".wrapper").classList.toggle("hide");
     document.getElementById("hgroup").classList.toggle("hide");
     document.querySelector(".component-picker").classList.toggle("noborder");
 }
