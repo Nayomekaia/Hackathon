25c9e5be710c11f2548d65b196b63243d99a570c
Melissa <melissa.blankenburgh@hva.nl>
2026-03-25 16:03:31 +0100

    de responsive code werkt nu alleen op kleine schermen

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -3,8 +3,4 @@ function toggleDropdown(){
     document.querySelector(".wrapper").classList.toggle("hide");
     document.getElementById("hgroup").classList.toggle("hide");
     document.querySelector(".component-picker").classList.toggle("noborder");
-
 }
-
-
-// document.getElementsByClassName(component-picker).addEventListener("mousedown", document.getElementById("form").classList.remove("dropdown-shown"));
