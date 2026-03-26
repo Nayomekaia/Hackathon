28a5a3125d73de2dbd697bc9d734e5187f408901
Nayomekaia <nayomedoelwijt@gmail.com>
2026-03-26 10:42:36 +0100

    achtergond feller gemaakt

--- src/setup/scene.js
+++ src/setup/scene.js
@@ -1,13 +1,22 @@
 import * as THREE from 'three';
 
-export function createScene() {
+export function createScene(renderer) {
     const scene = new THREE.Scene();
-
     const loader = new THREE.TextureLoader();
+
     loader.load('/galaxy.jpg', (texture) => {
-        texture.mapping = THREE.EquirectangularReflectionMapping;
         texture.colorSpace = THREE.SRGBColorSpace;
-        scene.background = texture;
+
+        const geometry = new THREE.SphereGeometry(60, 74, 64);
+
+        const material = new THREE.MeshBasicMaterial({
+            map: texture,
+            side: THREE.BackSide,
+            color: new THREE.Color(4.5, 4.5, 4.5) 
+        });
+
+        const sky = new THREE.Mesh(geometry, material);
+        scene.add(sky);
     });
 
     return scene;
